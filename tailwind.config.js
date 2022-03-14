063f04cfa493d26320d3576319ebd4e730c9cd7c
MichaelDepp <michaeldepp7@gmail.com>
2022-03-14 12:32:56 +0800

    fix: format the files

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
         './src/screens/**/*.{js,ts,jsx,tsx}',
         './src/components/**/*.{js,ts,jsx,tsx}'
     ],
+    darkMode: 'class',
     theme: {
         fontSize: {
             xs: '0.75rem',
