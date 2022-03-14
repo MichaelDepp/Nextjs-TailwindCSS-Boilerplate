a2d5fdde3ecf7142b5f8dc359a6a0acde1bfdda1
MichaelDepp <michaeldepp7@gmail.com>
2022-03-14 23:17:37 +0800

    feat: added the header to the container

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Home from '../screens/Home';
+import HeaderBar from '../components/HeaderBar';
 
 const Index = () => (
     <>
@@ -7,6 +8,7 @@ const Index = () => (
             <title>Next Js, Tailwind Css Boilerplate - Confetti Themes</title>
             <link rel="icon" href="/favicon.ico" />
         </Head>
+        <HeaderBar />
         <Home />
     </>
 );
