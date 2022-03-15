0ee0b478d192d193f735ef08e8ef5b0205e22abc
MichaelDepp <michaeldepp7@gmail.com>
2022-03-15 15:31:52 +0800

    refactor: fix the image changes

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,13 +1,14 @@
+import React from 'react';
 import Container from '../../components/Container';
 import ProfileBlob from '../../components/ProfileBlob';
 
 const Home = () => {
     return (
         <Container>
-            <h1 className="p-4 text-center text-xl text-theme-primary font-bold">
+            <h1 className="mt-12 p-4 text-center text-xl text-theme-primary font-bold">
                 This is a boilerplate with Next.js, Tailwind CSS
             </h1>
-            <div className="mt-32 lg:mt-80 flex justify-center">
+            <div className="mt-28 lg:mt-80 flex justify-center">
                 <ProfileBlob image={'./assets/astronaut.png'} />
             </div>
             <h1 className="fixed bottom-0 p-4 w-full text-center text-sm text-theme-primary">
